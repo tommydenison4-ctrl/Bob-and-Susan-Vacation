2b1e0a5a41343a229bb5f6ef65bab94a28a0ca32
tommydenison4-ctrl <tommydenison4@gmail.com>
2026-09-25 04:12:00 -0400

    Refresh service worker immediately

--- components/RegisterSW.tsx
+++ components/RegisterSW.tsx
@@ -1,3 +1,3 @@
 'use client';
 import {useEffect} from 'react';
-export default function RegisterSW(){useEffect(()=>{if('serviceWorker' in navigator)navigator.serviceWorker.register('/sw.js').catch(()=>{})},[]);return null}
+export default function RegisterSW(){useEffect(()=>{if(!('serviceWorker' in navigator))return;navigator.serviceWorker.register('/sw.js',{updateViaCache:'none'}).then(reg=>reg.update()).catch(()=>{})},[]);return null}
